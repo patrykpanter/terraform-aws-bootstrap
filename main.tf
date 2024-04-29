3f0292ee0383adb35c6ff9b7437b8d12947d39c4
Patryk Panter <panterpatryk@gmail.com>
2024-04-29 08:51:11 +0200

    Added DynamoDB table for state locks

--- main.tf
+++ main.tf
@@ -41,3 +41,14 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
     }
   }
 }
+
+# DynamoDB table for storing state locks
+resource "aws_dynamodb_table" "terraform_locks" {
+    name = var.dynamodb_table
+    billing_mode = "PAY_PER_REQUEST"
+    hash_key = "LockID"
+    attribute {
+        name = "LockID"
+        type = "S"
+    }
+}
